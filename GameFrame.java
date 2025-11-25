ffc9a4c6333f2f5922e5dc063f5ca136570da51f
SaieelM <saieelmalvankar07@gmail.com>
2025-11-26 00:52:55 +0530

    Initial commit

---
+++ GameFrame.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+import java.awt.Color;
+
+public class GameFrame extends JFrame {
+
+    GamePanel panel;
+
+    public GameFrame() {
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Pong Game");
+        this.setResizable(false);
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
